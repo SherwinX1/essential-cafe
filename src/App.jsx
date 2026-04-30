113d6a229d14f21c5f63b23218f5e711a1a8cdde
Sherwin Lopez <lopezsherwin.work@gmail.com>
2026-04-30 11:12:46 +0800

    react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-slate-900">
+      <h1 className="text-4xl font-bold text-sky-400 underline">
+        React + Tailwind v4 is live!
+      </h1>
+    </div>
+  )
+}
+
+export default App
